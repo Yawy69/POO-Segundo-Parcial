f4e257841eac8f0e44e92ee0e2768816e3bf6320
Yawy Angelito <150443493+Yawy69@users.noreply.github.com>
2024-01-23 10:23:00 -0500

    Add files via upload

---
+++ Restaurante_Mariscos/src/Restaurante/TotalesPagar.java
@@ -0,0 +1,31 @@
+
+package Restaurante;
+
+/**
+ *
+ * @author Yannick
+ */
+
+
+final public class TotalesPagar{
+  
+     static private int precioTotal;
+     
+    
+   public TotalesPagar (int precioTotal) {
+        this.precioTotal = precioTotal;
+    }
+
+    public TotalesPagar() {
+    
+    }
+
+    public static int getPrecioTotal() {
+        return precioTotal;
+    }
+
+    public static void setPrecioTotal(int precioTotal) {
+        TotalesPagar.precioTotal = precioTotal;
+    }
+
+}
